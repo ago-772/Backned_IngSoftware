543869ec063a779a9622bba5d76f0e9257a603e6
ago-772 <agostina.rocio.martin@mi.unc.edu.ar>
2026-06-08 00:33:54 -0300

    repositories

--- backend/spring/final-project/src/main/java/com/iot/repositories/EventRepository.java
+++ backend/spring/final-project/src/main/java/com/iot/repositories/EventRepository.java
@@ -1,14 +1,13 @@
 package com.iot.repositories;
 
 import com.iot.models.entities.EventEntity;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional; 
-
 @Repository
 public interface EventRepository extends JpaRepository<EventEntity, Long> {
-    
-    //Returns the most recent event of a given type.
-    Optional<EventEntity> findTopByOrderByTimestampDesc();
+
+  // Returns the most recent event of a given type.
+  Optional<EventEntity> findTopByOrderByTimestampDesc();
 }
--- backend/spring/final-project/src/main/java/com/iot/repositories/MateSessionRepository.java
+++ backend/spring/final-project/src/main/java/com/iot/repositories/MateSessionRepository.java
@@ -2,12 +2,10 @@ package com.iot.repositories;
 
 import com.iot.models.entities.MateSessionEntity;
 import com.iot.models.enums.SessionType;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-public interface MateSessionRepository
-        extends JpaRepository<MateSessionEntity, Long> {
-        //Search the last session active (SYSTEM_STARTED)
-        Optional<MateSessionEntity> findFirstBySessionType(SessionType sessionType);
+public interface MateSessionRepository extends JpaRepository<MateSessionEntity, Long> {
+  // Search the last session active (SYSTEM_STARTED)
+  Optional<MateSessionEntity> findFirstBySessionType(SessionType sessionType);
 }
